dc164c1c728ea937e6efe0a0b30a0bded6d37135
Ian <ianlehman11@hotmail.com>
2025-09-09 21:46:45 -0300

    Proyecto creado

---
+++ src/main/java/com/ilehmann/springboot/cursobackend/Main.java
@@ -0,0 +1,13 @@
+package com.ilehmann.springboot.cursobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilehmann/springboot/cursobackend/CursoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilehmann.springboot.cursobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
